5ed7fc0ce9332f56326b174b3dd4cce61a45ee38
Janet Leon <leon.irenejanet@gmail.com>
2018-09-04 14:29:43 -0700

    set up GET notes list id endpoint

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-'use strict';
+const express = require('express');
 
-// Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+app.listen(8080, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+    
+
+// ADD STATIC SERVER HERE
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+  //const if = req.params.id; LINE 22 is the same as this
+  let requestedData = data.find(item => item.id === Number(id));
+
+
+//   for (let i = 0 ; i < data.length; i++){
+//     if (data[i].id=== Number(id)){
+//       requestedData = data[i];
+//     }
+//   }
+
+   res.json(requestedData);
+
+});
+
+
