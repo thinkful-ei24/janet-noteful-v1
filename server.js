ba52d230eba9c69fc66269371ac122dec3a2d504
Janet Leon <leon.irenejanet@gmail.com>
2018-09-05 11:37:11 -0700

    added congig.js with updated PORT function

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const data = require('./db/notes');
 
 const app = express();
 
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
+const { PORT } = require('./config');
+
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
     
 
